c444dbb0649600b552e8bc8a269524862b336c38
Treaxy <146599871+Treaxy@users.noreply.github.com>
2024-02-09 19:44:09 +0300

    Add files via upload

---
+++ AnomalyHub 1.1.1/AnomalyHub/Config.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace AnomalyHub
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin is Enabled?")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Plugin Debug is Enabled?")]
+        public bool Debug { get; set; } = false;
+        [Description("Chaos spawn message (cassie style)")]
+        public string CIspawnMessage { get; set; } = "Chaos Insurgency HasEntered from Gate Alpha";
+        [Description("Round start message (cassie style)")]
+        public string RoundStartMessage { get; set; } = "pitch_0.2 .g4 .g4 pitch_0.9 The Site is Experiencing Many Keter and Euclid Level SCP Containment Breaches. Full Site Lock down Initiated. pitch_1.0";
+        [Description("Round is ended Friendly Fire will be open (true or false), dont use caps")]
+        public string AutoFF { get; set; } = "true";
+    }
+}
---
+++ AnomalyHub 1.1.1/AnomalyHub/EventHandlers.cs
@@ -0,0 +1,87 @@
+﻿using Exiled.Events.EventArgs.Server;
+using System;
+using MEC;
+using Respawning;
+using UnityEngine;
+using Cassie = Exiled.API.Features.Cassie;
+using Map = Exiled.API.Features.Map;
+using Server = Exiled.API.Features.Server;
+
+namespace AnomalyHub
+{
+    public class EventHandlers
+    {
+        string AutoFF1s = Plugin.Instance.Config.AutoFF;
+        public void AutoFF(RoundEndedEventArgs ev)
+        {
+            try
+            {
+                if (AutoFF1s == "true")
+                {
+                    Map.Broadcast(5, "FriendlyFire Active");
+                    Server.FriendlyFire = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Map.Broadcast(60, $"AnomalyHub plugin error: {e.Message.ToString().ToUpper()}");
+            }
+        }
+
+        public void Spawn(RespawningTeamEventArgs ev)
+        {
+            try
+            {
+                string CIspawnMessage = Plugin.Instance.Config.CIspawnMessage.Replace("Chaos Insurgency HasEntered from Gate Alpha", string.Copy(Plugin.Instance.Config.CIspawnMessage));
+
+                if (ev.NextKnownTeam == SpawnableTeamType.NineTailedFox)
+                {
+                    Timing.CallDelayed(1f, () => { Map.ChangeLightsColor(Color.clear); });
+                    Timing.CallDelayed(2f, () => { Map.ChangeLightsColor(Color.blue); });
+                    Timing.CallDelayed(3f, () => { Map.ChangeLightsColor(Color.clear); });
+                    Timing.CallDelayed(4f, () => { Map.ChangeLightsColor(Color.blue); });
+                    Timing.CallDelayed(5f, () => { Map.ChangeLightsColor(Color.clear); });
+                }
+                
+                else
+                {
+                    Timing.CallDelayed(1f, () => { Map.ChangeLightsColor(Color.clear); });
+                    Timing.CallDelayed(2f, () => { Map.ChangeLightsColor(Color.green); });
+                    Timing.CallDelayed(3f, () => { Map.ChangeLightsColor(Color.clear); });
+                    Timing.CallDelayed(4f, () => { Map.ChangeLightsColor(Color.green); });
+                    Timing.CallDelayed(5f, () => { Map.ChangeLightsColor(Color.clear); });
+                    Cassie.Message(CIspawnMessage, true, true, true);
+                }
+            }
+            catch (Exception e)
+            {
+                Map.Broadcast(60, $"AnomalyHub plugin error: {e.Message.ToString().ToUpper()}");
+            }
+        }
+
+        public void OnRoundStarted()
+        {
+            try
+            {
+                string RoundStartMessage = Plugin.Instance.Config.RoundStartMessage;
+                Map.ChangeLightsColor(Color.red);
+                Cassie.Message(RoundStartMessage, true, true, true);
+                Timing.CallDelayed(30f, () =>
+                {
+                    Map.ChangeLightsColor(Color.clear); 
+                });
+
+                if (AutoFF1s == "true")
+                {
+                    Server.FriendlyFire = false;
+                }
+            }
+
+            catch (Exception e)
+            {
+                Map.Broadcast(60,
+                    $"AnomalyHub plugin error: {e.Message.ToString().ToUpper()}");
+            }
+        }
+    }
+}
